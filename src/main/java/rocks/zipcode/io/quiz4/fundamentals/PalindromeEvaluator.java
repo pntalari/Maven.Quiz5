89d0d68699a7415d34035f40005937957565c969
Nirmala <nirmala.talari@gmail.com>
2019-04-08 09:58:59 -0400

    Fundamentals-PalindromeDone

--- src/main/java/rocks/zipcode/io/quiz4/fundamentals/PalindromeEvaluator.java
+++ src/main/java/rocks/zipcode/io/quiz4/fundamentals/PalindromeEvaluator.java
@@ -1,18 +1,38 @@
 package rocks.zipcode.io.quiz4.fundamentals;
 
+import java.sql.Array;
+import java.util.*;
+
 /**
  * @author leon on 18/12/2018.
  */
 public class PalindromeEvaluator {
     public static String[] getAllPalindromes(String string) {
-        return null;
+        List<String> subStr = Arrays.asList(StringEvaluator.getAllSubstrings(string));
+        List<String> retStr = new ArrayList<>();
+
+        for (int i = 0; i <subStr.size() ; i++) {
+            if(isPalindrome(subStr.get(i))){
+                retStr.add(subStr.get(i));
+            }
+        }
+
+        return retStr.toArray(new String[retStr.size()]);
+
     }
 
     public static Boolean isPalindrome(String string) {
-        return null;
+        if (string.equals(reverseString(string)))
+            return true;
+        else
+            return false;
     }
 
     public static String reverseString(String string) {
-        return null;
+        StringBuilder builder = new StringBuilder();
+        builder.append(string, 0, string.length());
+        String reversed = builder.reverse().toString();
+
+        return reversed;
     }
 }
