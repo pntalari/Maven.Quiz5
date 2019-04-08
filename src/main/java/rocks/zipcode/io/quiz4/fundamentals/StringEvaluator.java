36fda93909d65828144352c5117289949646a974
Nirmala <nirmala.talari@gmail.com>
2019-04-08 09:45:41 -0400

    Fundamentals-StringEvaluatorDone

--- src/main/java/rocks/zipcode/io/quiz4/fundamentals/StringEvaluator.java
+++ src/main/java/rocks/zipcode/io/quiz4/fundamentals/StringEvaluator.java
@@ -1,18 +1,42 @@
 package rocks.zipcode.io.quiz4.fundamentals;
 
+import java.awt.print.Pageable;
+import java.util.*;
+
 /**
  * @author leon on 11/12/2018.
  */
 public class StringEvaluator {
     public static String[] getAllSubstrings(String string) {
-        return null;
+        Set<String> subStr = new HashSet<>();
+
+        for (int i = 0; i < string.length(); i++) {
+            for (int j = i + 1; j <= string.length(); j++) {
+                subStr.add(string.substring(i, j));
+            }
+        }
+
+        return subStr.toArray(new String[subStr.size()]);
     }
 
     public static String[] getCommonSubstrings(String string1, String string2) {
-        return null;
+        List<String> str1Sub = Arrays.asList(getAllSubstrings(string1));
+        List<String> str2Sub = Arrays.asList(getAllSubstrings(string2));
+
+        Set<String> retStr = new HashSet<>();
+
+        for (int i = 0; i < str2Sub.size(); i++) {
+            if (str1Sub.contains(str2Sub.get(i))){
+                retStr.add(str2Sub.get(i));
+            }
+        }
+        return retStr.toArray(new String[retStr.size()]);
     }
 
     public static String getLargestCommonSubstring(String string1, String string2) {
-        return null;
+        List<String> strCommon = Arrays.asList(getCommonSubstrings(string1,string2));
+        String max = Collections.max(strCommon,Comparator.comparing(String::length));
+
+        return max;
     }
 }
